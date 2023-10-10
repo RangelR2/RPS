64f3c6d236a367548a24f07dccaf70724f9f3fa1
RangelR2 <grangellemes@gmail.com>
2023-10-09 21:13:57 -0300

    Mais funções

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-let teste;
-
-function letsplay(){
+function getComputerChoice(){
     let op = ["Rock", "Paper", "Scissor"];
     let random = Math.floor(Math.random() * op.length);
     let teste = op[random];
     console.log("Your oponent played: " + teste);
-    return teste;
 }
 
-letsplay();
+function playRound(playerSelection, computerSelection){
 
-console.log(teste)
+}
 
+let playerSelection = prompt("Make your choice!");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+console.log(playerSelection)
+function game(){
 
-
+}
