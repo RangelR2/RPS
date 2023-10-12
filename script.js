5a4848cb4d7269569e1adab8b614fd168304d5c1
RangelR2 <grangellemes@gmail.com>
2023-10-12 11:12:17 -0300

    Round sistem

--- script.js
+++ script.js
@@ -1,26 +1,50 @@
-let option = prompt("What is your choice?\nRock, Paper, Scissor");
-let playerSelection = option.toLowerCase();
-const myArray = ["Rock", "Paper", "Scissor"];
+let option = prompt("What is your choice?\nRock, Paper, Scissor").toLowerCase();
+const myArray = ["rock", "paper", "scissor"];
 let randomChoise;
 
+if(option == "rock" || option == "paper" || option == "scissor"){
+    console.log("You played: " + option);    
+}else{
+    console.log("This isn't a option!");
+}
 
 function getComputerChoice(){
     let random = Math.floor(Math.random() * myArray.length);
     randomChoise = myArray[random];
     console.log("Your oponent played: " + randomChoise);
+    console.log("--------------------------------");
     return randomChoise;
 }
 
-function round(playerSelection, getComputerChoice){
-    if(playerSelection === getComputerChoice){
-        console.log("Empate!");
-    }else{
-        console.log("pfv aparece");
+function round(){
+    if(option == randomChoise){
+        console.log("Draw!");
     }
-    return;
+    if(option == "rock" && randomChoise == "scissor"){
+        console.log("You won!")
+    }else{
+    }
+    if(option == "rock" && randomChoise == "paper"){
+        console.log("You lost!")
+    }else{
+    }
+    if(option == "paper" && randomChoise == "scissor"){
+        console.log("You lost!")
+    }else{
+    }
+    if(option == "paper" && randomChoise == "rock"){
+        console.log("You won!")
+    }else{
+    }
+    if(option == "scissor" && randomChoise == "rock"){
+        console.log("You lost!")
+    }else{
+    }
+    if(option == "scissor" && randomChoise == "paper"){
+        console.log("You won!")
+    }else{
+    }
 }
 
-console.log(getComputerChoice());
-console.log(round());
-console.log("Teste: " + playerSelection)
-
+getComputerChoice();
+round();
