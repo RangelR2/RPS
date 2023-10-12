91622e46ece8114b557b474096e4f1680c3b7fc2
RangelR2 <grangellemes@gmail.com>
2023-10-11 23:03:04 -0300

    Att

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+let yourChoice = prompt("What is your choice?\nRock, Paper, Scissor")
+let teste;
+
+let opChoice;
+
 function getComputerChoice(){
     let op = ["Rock", "Paper", "Scissor"];
     let random = Math.floor(Math.random() * op.length);
     let teste = op[random];
     console.log("Your oponent played: " + teste);
+    return teste;
 }
 
-function playRound(playerSelection, computerSelection){
-
+function round(playerSelection, computerSelection){
+    
 }
 
-let playerSelection = prompt("Make your choice!");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-console.log(playerSelection)
-function game(){
-
-}
+getComputerChoice();
+console.log(yourChoice);
+console.log(teste);
