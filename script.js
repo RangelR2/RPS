1fa2276f8b45aae276342a6bbe3ef90c0eea8621
RangelR2 <grangellemes@gmail.com>
2023-10-21 00:53:58 -0300

    Projeto com totalmente funcional

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const middleDiv = document.createElement('middleDiv');
 const bottomDiv = document.createElement('bottomDiv');
 
 const upperMessage = document.createElement('p');
+const upperMessage2 = document.createElement('p');
 const bottomMessage = document.createElement('p');
 
 document.querySelector('#rock').disabled = true;
@@ -22,71 +23,63 @@ document.querySelector('#scissor').disabled = true;
 const startDiv = document.createElement('startDiv');
 startDiv.classList.add('div');
 scoreAll.appendChild(startDiv);
-startDiv.setAttribute('style', 'border: 2px solid black; display: flex; height: 100%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+startDiv.setAttribute('style', 'font-family: "Roboto", sans-serif; border: 2px solid black; display: flex; height: 100%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
 const startButton = document.createElement('button');
 startButton.classList.add('startButton');
 startButton.textContent = 'Start!';
 startDiv.appendChild(startButton);
-startButton.setAttribute('style', 'background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+startButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
 startButton.addEventListener('mouseover', () => {
-startButton.setAttribute('style', 'background-color: rgb(189, 186, 186); padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+    startButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: rgb(189, 186, 186); padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 });
 
 startButton.addEventListener('mouseout', () => {
-    startButton.setAttribute('style', 'background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer;align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+    startButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer;align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 });
 
 startButton.addEventListener('click', () => {
+
+    bottomMessage.textContent = 'First to score 5 point win!';
     startDiv.remove();
     startButton.remove();
     document.querySelector('#rock').disabled = false;
     document.querySelector('#paper').disabled = false;
     document.querySelector('#scissor').disabled = false;
 
-    buttonChoices();
-    renderScore();
-
-    
     upperDiv.classList.add('div');
     scoreAll.appendChild(upperDiv);
     upperDiv.setAttribute('style', 'display: flex; height: 33%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
-    
     middleDiv.classList.add('div');
     scoreAll.appendChild(middleDiv);
-    middleDiv.setAttribute('style','display: flex; justify-content: center; height: 31%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+    middleDiv.setAttribute('style', 'display: flex; justify-content: center; height: 31%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
-    
     bottomDiv.classList.add('div');
     scoreAll.appendChild(bottomDiv);
     bottomDiv.setAttribute('style', 'display: flex; height: 33%; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
 
     upperMessage.classList.add('upperMessage');
     upperDiv.appendChild(upperMessage);
-    upperMessage.textContent = 'aoba';
-    upperMessage.setAttribute('style','color: white;')
+    upperMessage.setAttribute('style', 'font-family: "Roboto", sans-serif; font-size: 20px; color: white;');
 
     bottomMessage.classList.add('bottomMessage');
     bottomDiv.appendChild(bottomMessage);
-    bottomMessage.setAttribute('style','color: white;')
+    bottomMessage.setAttribute('style', 'font-family: "Roboto", sans-serif; font-size: 20px; color: white;')
 
     scoreYou.classList.add('scoreYou');
     scoreYou.textContent = 'You: ' + scorePlayer;
     middleDiv.appendChild(scoreYou);
-    scoreYou.setAttribute('style', 'color: white;');
+    scoreYou.setAttribute('style', 'font-family: "Roboto", sans-serif; color: white;');
 
     scoreOp.classList.add('scoreOp');
     scoreOp.textContent = 'Opponent: ' + scoreOpponent;
     middleDiv.appendChild(scoreOp);
-    scoreOp.setAttribute('style', 'color: orange;');
+    scoreOp.setAttribute('style', 'font-family: "Roboto", sans-serif; color: orange;');
+
+    buttonChoices();
 });
-
-/// Ui Score
-
-function renderScore(){
-}
 
 /// Choice Buttons
 
@@ -115,21 +108,27 @@ function buttonChoices() {
 function playRound() {
     getComputerChoice();
     if (playerSelection == randomChoise) {
-        bottomMessage.textContent = 'Tie';
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        randomChoise = randomChoise.charAt(0).toUpperCase() + randomChoise.slice(1);
+        bottomMessage.textContent = "It's a draw! No one earn points!";
         winner = "both";
         scoreteste();
     }
     if ((playerSelection == "rock" && randomChoise == "scissor") ||
         (playerSelection == "paper" && randomChoise == "rock") ||
         (playerSelection == "scissor" && randomChoise == "paper")) {
-        bottomMessage.textContent = 'Player choose ' + playerSelection + ' and win this round!';
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        randomChoise = randomChoise.charAt(0).toUpperCase() + randomChoise.slice(1);
+        bottomMessage.textContent = playerSelection + ' beats ' + randomChoise + '!';
         winner = "player";
         scoreteste();
     }
     if ((playerSelection == "rock" && randomChoise == "paper") ||
         (playerSelection == "paper" && randomChoise == "scissor") ||
         (playerSelection == "scissor" && randomChoise == "rock")) {
-        bottomMessage.textContent = 'Player choose ' + playerSelection + ' and lose this round!';
+        playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        randomChoise = randomChoise.charAt(0).toUpperCase() + randomChoise.slice(1);
+        bottomMessage.textContent = playerSelection + ' is beaten by ' + randomChoise + '!';
         winner = "bot";
         scoreteste();
     }
@@ -140,7 +139,6 @@ function playRound() {
 function getComputerChoice() {
     let random = Math.floor(Math.random() * myArray.length);
     randomChoise = myArray[random];
-    console.log("Your opponent played: " + randomChoise);
     return randomChoise;
 }
 
@@ -148,25 +146,15 @@ function getComputerChoice() {
 
 function scoreteste() {
     if (winner == "both") {
-        scoreOpponent++;
-        scorePlayer++;
-        console.log("Bot: " + scoreOpponent);
-        console.log("Player: " + scorePlayer);
         scoreBoard()
     }
     if (winner == "player") {
         scorePlayer++;
-        console.log("Bot: " + scoreOpponent);
-        console.log("Player: " + scorePlayer);
         scoreBoard()
-        return scorePlayer;
     }
     if (winner == "bot") {
         scoreOpponent++;
-        console.log("Bot: " + scoreOpponent);
-        console.log("Player: " + scorePlayer);
         scoreBoard()
-        return scoreOpponent;
     }
 }
 
@@ -176,63 +164,76 @@ function scoreBoard() {
     scoreYou.classList.add('scoreYou');
     scoreYou.textContent = 'You: ' + scorePlayer;
     middleDiv.appendChild(scoreYou);
-    scoreYou.setAttribute('style', 'color: white;');    
+    scoreYou.setAttribute('style', 'font-family: "Roboto", sans-serif; color: white;');
 
     scoreOp.classList.add('scoreOp');
     scoreOp.textContent = 'Opponent: ' + scoreOpponent;
     middleDiv.appendChild(scoreOp);
-    scoreOp.setAttribute('style', 'color: orange;');
+    scoreOp.setAttribute('style', 'font-family: "Roboto", sans-serif; color: orange;');
 
-    if(scoreOpponent == 5){
+    if (scoreOpponent === 5) {
         bottomMessage.textContent = 'You Lost!'
+        document.querySelector('#rock').disabled = true;
+        document.querySelector('#paper').disabled = true;
+        document.querySelector('#scissor').disabled = true;
         playAgain();
-        scoreOpponent = 0;
-        scorePlayer = 0;
     }
 
-    if (scorePlayer == 5) {
+    if (scorePlayer === 5) {
         bottomMessage.textContent = 'You Won!'
-        playAgain();        
-        scoreOpponent = 0;
-        scorePlayer = 0;
+        document.querySelector('#rock').disabled = true;
+        document.querySelector('#paper').disabled = true;
+        document.querySelector('#scissor').disabled = true;
+        playAgain();
+    }
+
+    if (scorePlayer === 5 && scoreOpponent === 5) {
+        bottomMessage.textContent = 'Tie!';
+        document.querySelector('#rock').disabled = true;
+        document.querySelector('#paper').disabled = true;
+        document.querySelector('#scissor').disabled = true;
+        playAgain();
     }
 }
 
 /// Reset Function
 
 function playAgain() {
+    upperMessage.textContent = 'Final Score: ' + scorePlayer + ' x ' + scoreOpponent;
     scoreYou.remove();
     scoreOp.remove();
     const resetButton = document.createElement('button');
     resetButton.classList.add('resetButton');
     resetButton.textContent = 'Play again!';
     middleDiv.appendChild(resetButton);
-    resetButton.setAttribute('style', 'background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;'); 
+    resetButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+
+    resetButton.addEventListener('mouseover', () => {
+        resetButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: rgb(189, 186, 186); padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer; align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+    });
+
+    resetButton.addEventListener('mouseout', () => {
+        resetButton.setAttribute('style', 'font-family: "Roboto", sans-serif; background-color: white; padding:20px; padding-left: 30px; padding-right: 30px; border-radius: 15px;font-size: 20px; cursor: pointer;align-items: center; flex-direction: row; flex-wrap: wrap; justify-content: space-around;');
+    });
 
     resetButton.addEventListener('click', () => {
-        
+        upperMessage.textContent = '';
+        document.querySelector('#rock').disabled = false;
+        document.querySelector('#paper').disabled = false;
+        document.querySelector('#scissor').disabled = false;
         scoreOpponent = 0;
         scorePlayer = 0;
+        playerSelection = "";
+        randomChoise = "";
+        bottomMessage.textContent = 'First to score 5 point win!';
         scoreYou.classList.add('scoreYou');
-        scoreYou.textContent = 'You: ' + scorePlayer;
+        scoreYou.textContent = `You: ${scorePlayer}`;
         middleDiv.appendChild(scoreYou);
-        scoreYou.setAttribute('style', 'color: white;');
-    
+        scoreYou.setAttribute('style', 'font-family: "Roboto", sans-serif; color: white;')
         scoreOp.classList.add('scoreOp');
-        scoreOp.textContent = 'Opponent: ' + scoreOpponent;
+        scoreOp.textContent = `Opponent: ${scoreOpponent}`
         middleDiv.appendChild(scoreOp);
-        scoreOp.setAttribute('style', 'color: orange;');
-
+        scoreOp.setAttribute('style', 'font-family: "Roboto", sans-serif; color: orange;');
         resetButton.remove();
-        buttonChoices();
     });
 }
-
-
-
-
-
-
-
-
-
